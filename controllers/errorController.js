ff4111e292ba8df8bf165003e354538d258c2e46
Koda-Black <onuorah2kingsley@gmail.com>
2023-10-12 07:40:24 +0100

    Stripe bug fix-2

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -88,6 +88,7 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
+    //REMEMBER THIS /////
     // sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
@@ -102,6 +103,8 @@ module.exports = (err, req, res, next) => {
 
     // console.log(err.message);
     // console.log(error.message);
-    sendErrorProd(err, req, res);
+
+    //REMEMBER THIS /////
+    // sendErrorProd(err, req, res);
   }
 };
